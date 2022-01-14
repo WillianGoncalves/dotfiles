1722d61d4e69848c3d5298d0171e62b1dcdef288
Willian Gonçalves <w.goncalves91@gmail.com>
2022-01-14 12:26:37 -0300

    [vim] Add terraform syntax highlight

--- vim/rcfiles/treesitter.lua
+++ vim/rcfiles/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "javascript", "scss" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "javascript", "scss", "hcl" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
