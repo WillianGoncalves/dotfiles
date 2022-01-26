9fcd325df8efc0852430dd236a97bfb4281ab33f
Willian Gonçalves <w.goncalves91@gmail.com>
2022-01-26 10:59:35 -0300

    [vim] Replace ale with coc-diagnostic

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ BASEDIR=$(pwd)
 
 #install dependencies
 sudo apt-get install build-essential cmake
+yarn global add diagnostic-languageserver
 
 # create neovim config folder
 mkdir -p ~/.config/nvim/
