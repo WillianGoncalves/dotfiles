83a8c8aecbc36a9b2b9276dbc2d9e431cc8e95a1
Willian Gonçalves <w.goncalves91@gmail.com>
2022-09-21 17:21:22 -0300

    [vim] Add init.lua in plugins-config

---
+++ vim/lua/plugins-config/init.lua
@@ -0,0 +1,10 @@
+-- lua configuration files
+require('plugins-config/cmp')
+require('plugins-config/closetag')
+require('plugins-config/easy-align')
+require('plugins-config/gitgutter')
+require('plugins-config/indent-line')
+require('plugins-config/nvim-tree')
+
+-- vim configuration files
+vim.cmd('source' .. vim.fn.stdpath('config') .. '/lua/plugins-config/lightline.vim')
