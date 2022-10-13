7ad04c339c70942d90ff69a9ce5379f5201dd469
Willian Gonçalves <w.goncalves91@gmail.com>
2022-10-13 15:20:49 -0300

    [vim] Add keymaps for indentation

--- vim/lua/keymaps.lua
+++ vim/lua/keymaps.lua
@@ -1,2 +1,6 @@
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>', ':nohlsearch<CR>', opts)
+vim.keymap.set('n', '<TAB>', '>>', opts)
+vim.keymap.set('n', '<S-TAB>', '<<', opts)
+vim.keymap.set('n', '>', '>>', opts)
+vim.keymap.set('n', '<', '<<', opts)
