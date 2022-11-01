24f5f667a2d86953393f0d47b32ad522b179ad84
Willian Gonçalves <w.goncalves91@gmail.com>
2022-10-31 21:19:50 -0300

    [vim] Fix gqq problem

--- vim/lua/autocommands.lua
+++ vim/lua/autocommands.lua
@@ -14,3 +14,13 @@ vim.api.nvim_create_autocmd({'FileType'}, {
   pattern = 'gitcommit',
   command = 'setlocal spell textwidth=72'
 })
+
+-- Some extensions break the internal VIM formatter.
+-- See: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131
+-- As a workaround, we disable it for all extensions, so we use internal
+-- formatting for bindings like gq.
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    vim.bo[args.buf].formatexpr = nil
+  end,
+})
