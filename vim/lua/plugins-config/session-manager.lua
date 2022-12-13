6d1c1a438d56d8ffa1f09147d337f8b2bf2d56d4
Willian Gonçalves <w.goncalves91@gmail.com>
2022-12-13 14:42:02 -0300

    [vim] Improve session_manager config

--- vim/lua/plugins-config/session-manager.lua
+++ vim/lua/plugins-config/session-manager.lua
@@ -1,11 +1,11 @@
---local autosession_status_ok, autosession = pcall(require, "auto-session")
---if not autosession_status_ok then
-  --return
---end
+local status_ok, session_manager = pcall(require, "session_manager")
+if not status_ok then
+  return
+end
 
 local Path = require('plenary.path')
 -- See default values in https://github.com/Shatur/neovim-session-manager#configuration
-require('session_manager').setup({
+session_manager.setup {
   sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
   path_replacer = '__', -- The character to which the path separator will be replaced for session files.
   colon_replacer = '++', -- The character to which the colon symbol will be replaced for session files.
@@ -17,7 +17,7 @@ require('session_manager').setup({
   },
   autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
   max_path_length = 80,  -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
-})
+}
 
-vim.keymap.set('n', '<F5>', '<cmd>SessionManager save_current_session<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<F5>', '<cmd>SessionManager save_current_session<CR><cmd>echo "session saved"<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<F8>', '<cmd>SessionManager load_current_dir_session<CR>', { noremap = true, silent = true })
