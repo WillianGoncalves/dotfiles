41adefa20f5305afd3802ce167e8eae9f7aec0c3
Willian Gonçalves <w.goncalves91@gmail.com>
2023-01-13 22:28:01 -0300

    [vim] Add keymap to paste without loose the yanked text

--- vim/lua/keymaps.lua
+++ vim/lua/keymaps.lua
@@ -2,3 +2,5 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '\\', ':nohlsearch<CR>', opts)
 vim.keymap.set('n', '>', '>>', opts)
 vim.keymap.set('n', '<', '<<', opts)
+-- Paste above another content with <leader>p to keep the yanked text.
+vim.keymap.set('x', '<leader>p', '"_dP')
