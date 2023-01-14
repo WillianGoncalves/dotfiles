e8e5eff33be63392de1fe08aca237cebabe7b3ee
Willian Gonçalves <w.goncalves91@gmail.com>
2023-01-13 22:26:12 -0300

    [vim] Change key to disable highlighted search

--- vim/lua/keymaps.lua
+++ vim/lua/keymaps.lua
@@ -1,4 +1,4 @@
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>', ':nohlsearch<CR>', opts)
+vim.keymap.set('n', '\\', ':nohlsearch<CR>', opts)
 vim.keymap.set('n', '>', '>>', opts)
 vim.keymap.set('n', '<', '<<', opts)
