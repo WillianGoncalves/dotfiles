ee49056ee79afef71d180815e485e9c7ec59d1e2
Willian Gonçalves <w.goncalves91@gmail.com>
2023-09-29 12:12:34 -0300

    [vim] Update nvim-tree config

--- nvim/lua/plugins-config/nvim-tree.lua
+++ nvim/lua/plugins-config/nvim-tree.lua
@@ -1,72 +1,28 @@
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
-if not status_ok then
-  return
-end
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
--- following options are the default
-nvim_tree.setup {
-  -- disables netrw completely
-  disable_netrw       = true,
-  -- hijack netrw window on startup
-  hijack_netrw        = true,
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
-  -- updates the root directory of the tree on `DirChanged` (when you run `:cd` usually)
-  update_cwd          = false,
-  -- show lsp diagnostics in the signcolumn
-  diagnostics = {
-    -- enbables the feature
-    enable = false,
-  },
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+require("nvim-tree").setup({
   update_focused_file = {
-    -- enables the feature
-    enable      = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = false,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {}
+    enable = true,
   },
-  -- configuration options for the system open command (`s` in the tree by default)
-  system_open = {
-    -- the command to run this, leaving nil should work in most cases
-    cmd  = nil,
-    -- the command arguments as a list
-    args = {}
-  },
-
   view = {
-    -- width of the window, can be either a number (columns) or a string in `%`
     width = 40,
-    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
     side = 'left',
-    -- if true the tree will resize itself after opening a file
-    mappings = {
-      -- custom only false will merge the list with the default mappings
-      -- if true, it will only use your list to set the mappings
-      custom_only = false,
-      -- list of mappings to set on the tree manually
-      list = {}
-    }
   },
-
   actions = {
     open_file = {
       quit_on_open = true
     }
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = true,
   }
-}
+})
 
 opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<C-\\>', ':NvimTreeToggle<CR>', opts)
