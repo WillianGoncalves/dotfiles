8fdfd56dc6056d95b192a75fbdeff7ec82ed3a19
Willian Gonçalves <w.goncalves91@gmail.com>
2025-02-10 14:22:00 -0300

    [vim] Add the "vv" and "V" keymaps
    "vv" selects the entire line, like yy (copy the entire line) and dd
    (delete the entire line).
    
    "V" selects from the cursor until the end of the line, like Y and D.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -5,3 +5,9 @@ vim.keymap.set('n', '<', '<<', opts)
 -- Paste above another content with <leader>p to keep the yanked text.
 vim.keymap.set('x', '<leader>p', '"_dP')
 vim.keymap.set('n', 'U', ':Gread<CR>', opts)
+
+-- 'vv' select the entire line
+vim.api.nvim_set_keymap('n', 'vv', 'V', { noremap = true, silent = true })
+
+-- 'V' select from cursor to end of line
+vim.api.nvim_set_keymap('n', 'V', 'vg_', { noremap = true, silent = true })
