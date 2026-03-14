ae55053d63b5f2f2d2b2ba5072321c5c0f47963a
Willian Gonçalves <w.goncalves91@gmail.com>
2026-03-14 14:43:32 -0300

    [vim] Change default theme

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -10,6 +10,6 @@ return {
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme 'tokyonight-storm'
   end,
 }
